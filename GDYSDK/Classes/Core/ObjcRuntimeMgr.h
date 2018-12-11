a523d076a658ddaf005b0c6185f097fd5edcf577
AHZX <ahzx@iMac.local>
2018-12-11 17:24:44 +0800

    GDYSDK-12.11-update Runtime method

---
+++ GDYSDK/Classes/Core/ObjcRuntimeMgr.h
@@ -0,0 +1,16 @@
+//
+//  ObjcRuntimeMgr.h
+//  AFNetworking
+//
+//  Created by AHZX on 2018/12/11.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ObjcRuntimeMgr : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GDYSDK/Classes/Core/ObjcRuntimeMgr.m
@@ -0,0 +1,12 @@
+//
+//  ObjcRuntimeMgr.m
+//  AFNetworking
+//
+//  Created by AHZX on 2018/12/11.
+//
+
+#import "ObjcRuntimeMgr.h"
+
+@implementation ObjcRuntimeMgr
+
+@end
