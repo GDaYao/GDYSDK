c25c4a3c8e0fa145d4c185b2121ab18abd499541
GDayao <1418154979@qq.com>
2019-04-14 13:04:23 +0800

    commit 4.14

--- GDYSDK/Classes/Core/UICollectionView+CustomCV.h
+++ GDYSDK/Classes/Core/UICollectionView+CustomCV.h
@@ -3,6 +3,12 @@
 
 //function: 自定义UICollectionView,初始化并封装常用方法
 
+/**
+ 使用注意事项：
+    1. 使用identifier，在不同界面使用不同的UICollectionViewCell需要使用不同的cell identifier 特别在使用static修饰时，出现cellId重复使用（在内存中体现）。
+    2. 出现类似-UICollectionViewCell/UITableViewCell 'invalid nib registered for identifier (xxxViewCell) - nib must contain exactly one top level object which must be a UICollectionReusableView instance' 错误，是因为在xib文件中多一些没有添加到视图上的控件。
+ **/
+
 #import <UIKit/UIKit.h>
 
 @interface UICollectionView (CustomCV)
