583594139b079334b700631c89a3c82569025595
GDayao <1418154979@qq.com>
2019-10-15 19:27:57 +0800

    update network manager

--- GDYSDK/Classes/Core/GDYSDKNetworkMgr.m
+++ GDYSDK/Classes/Core/GDYSDKNetworkMgr.m
@@ -92,7 +92,8 @@ parameters:(id)parameters
 success:(void (^)(id _Nullable responseObject))success
 failure:(void (^)(NSError * _Nullable error))failure{
     AFHTTPSessionManager *sessionMgr = [AFHTTPSessionManager manager];
-    sessionMgr.responseSerializer = [AFHTTPResponseSerializer serializer];
+    // 下面这个属性，大多用在加密返回参数中使用
+    //sessionMgr.responseSerializer = [AFHTTPResponseSerializer serializer];
     sessionMgr.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"application/xhtml+xml", @"application/xml", @"text/html", @"text/json", @"text/plain", @"text/javascript", @"text/xml", @"image/*", @"video/mp4", @"text/plain",@"charset=utf-8",nil];
     sessionMgr.requestSerializer.timeoutInterval = 60.f;
     [sessionMgr POST:URLString parameters:parameters progress:^(NSProgress * _Nonnull downloadProgress) {
