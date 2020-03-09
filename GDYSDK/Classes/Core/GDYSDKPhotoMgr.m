c194f54d75765dee65db4dffffda2e73beb8dbe3
GDayao <1418154979@qq.com>
2020-03-09 19:30:03 +0800

    add get system album authorized

--- GDYSDK/Classes/Core/GDYSDKPhotoMgr.m
+++ GDYSDK/Classes/Core/GDYSDKPhotoMgr.m
@@ -7,7 +7,61 @@
 
 #import "GDYSDKPhotoMgr.h"
 
+#import <Photos/Photos.h>
+
 @implementation GDYSDKPhotoMgr
+
+#pragma mark - get album authorized status.
++ (void)getAuthorizationStatusAlbumWithAuthorized:(void(^)(BOOL authorized))authorizedBlock {
+    
+    PHAuthorizationStatus author = [PHPhotoLibrary authorizationStatus];
+    if (author == PHAuthorizationStatusAuthorized)
+    {
+        authorizedBlock(YES);
+    }else if (author == PHAuthorizationStatusNotDetermined)
+    {
+        // request album authorized.
+        [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (status == PHAuthorizationStatusAuthorized)
+                {
+                    authorizedBlock(YES);
+                }else{
+                    authorizedBlock(NO);
+                }
+            });
+        }];
+    }
+    else
+    {
+        UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"" message:@"未获得相册权限，进入设置开启权限" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"好的" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            // 跳转进入设置界面
+//            if ([UIDevice currentDevice].systemVersion.doubleValue >= 8.0 && [UIDevice currentDevice].systemVersion.doubleValue < 10.0) {  // iOS8.0 和 iOS9.0
+//                NSURL * url = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
+//                if ([[UIApplication sharedApplication] canOpenURL:url]) {
+//                    [[UIApplication sharedApplication] openURL:url];
+//                }
+//            }else if ([UIDevice currentDevice].systemVersion.doubleValue >= 10.0) {  // iOS10.0及以后
+//            }
+            NSURL * url = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
+            if ([[UIApplication sharedApplication] canOpenURL:url]) {
+                if (@available(iOS 10.0, *)) {
+                    [[UIApplication sharedApplication] openURL:url];
+                }
+            }
+        }];
+        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+            authorizedBlock(NO);
+        }];
+        [alertVC addAction:okAction];
+        [alertVC addAction:cancelAction];
+        UIWindow *topWindow = [UIApplication sharedApplication].keyWindow;
+        [topWindow.rootViewController presentViewController:alertVC animated:YES completion:nil];
+    }
+}
+
+
 
 
 #pragma mark - select photo from album
